e4299ab2f6973e00a47c61c59135a4e45bd15ef0
shalant <doug.rosenberg@gmail.com>
2024-03-22 18:59:57 -0500

    initial

---
+++ random-user-gen/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            // picture: 'https://randomuser.me/api/portaits/men/10.jpg'
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+
+        }
+    }
+})
+
+app.mount('#app')
