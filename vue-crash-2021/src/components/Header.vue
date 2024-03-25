9a6b21eedb29ad51636051dbd04b73256115a72b
shalant <doug.rosenberg@gmail.com>
2024-03-25 09:52:01 -0500

    finished

--- vue-crash-2021/src/components/Header.vue
+++ vue-crash-2021/src/components/Header.vue
@@ -1,34 +1,43 @@
 <template>
     <header>
-        <h1>{{  title }}</h1>
-        <Button @btn-click="$emit('toggle-add-task')" 
-            :text="showAddTask ? 'Close' : 'Add Task'" 
-            :color="showAddTask ? 'red' : 'green'"/>
+        <h1>{{ title }}</h1>
+        <Button v-show="homePage" @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'"
+            :color="showAddTask ? 'red' : 'green'" />
 
     </header>
 </template>
 
 <script>
-    import Button from './Button'
+import Button from './Button'
 
-    export default {
-        name: 'Header',
-        props: {
-            title: String,
-            showAddTask: Boolean
-        },
-        components: {
-            Button
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    components: {
+        Button,
+    },
+    computed: {
+        homePage() {
+            if (this.$route.path === '/') {
+                return true
+            } else {
+                return false
+            }
         }
     }
+}
 </script>
 
 <style scoped>
-    header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 20px;
-    }
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
 </style>
